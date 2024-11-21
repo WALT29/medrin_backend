3779f42a36efd82b395c70cf6178923ce25bd9de
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-11-21 06:01:20 +0300

    Final Com

--- server/routes/subscriptionRoutes.py
+++ server/routes/subscriptionRoutes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,request,make_response
 from flask_restful import Api,Resource
-from models import db,Subscription
+from models import db,Subscription,Organisation,Plans
+from uuid import UUID
 
 subscription_bp=Blueprint('subscription_bp',__name__,url_prefix='/subscription_route')
 api=Api(subscription_bp)
@@ -14,7 +15,41 @@ class Subscriptions(Resource):
                 "error":"No subscription is found"
             },404)
         
+       
+        
         return make_response(subscriptions,200)
+    
+    def post(self):
+        data=request.get_json()
+        user_id=data['user_id']
+        plan_id=data['plan_id']
+        
+        if isinstance(user_id,str) or isinstance(plan_id,str):
+            user_uuid=UUID(user_id)
+            plan_uuid=UUID(plan_id)
+        else:
+            plan_uuid = plan_id
+            user_uuid=user_id
+        
+        organisation=Organisation.query.filter_by(user_id=user_uuid).first()
+        plan=Plans.query.filter_by(id=plan_uuid).first()
+        
+        
+        if not organisation:
+            return make_response({
+                "message":"Enter the correct user id"
+            },404)
+        
+        if not plan:
+            return make_response({
+                "message":"Enter the correct plan id"
+            },404)
+        
+        new_sub=Subscription(organization_id=organisation.id,plan_id=plan_uuid)
+        db.session.add(new_sub)
+        db.session.commit()
+        
+        return make_response(new_sub.to_dict(),200)
 
 api.add_resource(Subscriptions,'/subscriptions')
 
